b94572467ffc4b85e845dfad92001cef0712ba9f
ejazalam11 <alamejaz72@gmail.com>
2025-05-02 13:29:02 +0500

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import Layout from './components/Layout/Layout.vue';
+import Layout from './components/layout/defult.vue';
+// @ts-ignore
 import Dashboard from './components/Dashboard/Dashboard.vue';
 </script>
 
